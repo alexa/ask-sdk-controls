e9ed7b651ff2aad7fcc0e0305c0133e1fd7f7fea
Shreyas Govinda Raju <shreraju@amazon.com>
2021-02-16 15:20:22 -0800

    fix_(listControl): removed ununsed def

--- src/commonControls/listControl/ListControlAPL.ts
+++ src/commonControls/listControl/ListControlAPL.ts
@@ -14,11 +14,6 @@ import { AplContent, ListControl } from './ListControl';
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
-interface APLListItem {
-    primaryText: string;
-}
-
 export namespace ListControlAPLPropsBuiltIns {
     export interface DefaultSelectValueAPLProps {
         /**
